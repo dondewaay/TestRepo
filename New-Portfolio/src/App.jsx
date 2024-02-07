7f6e527c9f32c1ad7e90a930328917da8395a1bf
ddewaay <ddewaay@uiowa.edu>
2024-02-07 16:02:58 -0600

    Got it

--- New-Portfolio/src/App.jsx
+++ New-Portfolio/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.scss";
 function App() {
   return (
     <>
-    <Border><Hero /></Border>
+      <Border />
     </>
   );
 }
--- New-Portfolio/src/components/Hero/Hero.jsx
+++ New-Portfolio/src/components/Hero/Hero.jsx
@@ -1,9 +1,5 @@
 import "./Hero.scss";
 
 export default function Hero() {
-  return (
-    <div className="hero">
-      <h2>I'm Don</h2>
-    </div>
-  );
+  return;
 }
