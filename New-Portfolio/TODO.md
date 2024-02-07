f5ff1efcccff5f871eef782c848a0c42b9bfc000
ddewaay <ddewaay@uiowa.edu>
2024-02-07 16:21:52 -0600

    Yup

---
+++ New-Portfolio/TODO.md
@@ -0,0 +1,4 @@
+Plan out on and look at cards
+add star symbols
+add scroll back to top arrow at bottom right
+DIII -> Don DeWaay III when hover
--- New-Portfolio/src/components/Border/Border.jsx
+++ New-Portfolio/src/components/Border/Border.jsx
@@ -4,7 +4,7 @@ export default function Border() {
   return (
     <>
       <div className="top">
-        <h1>DON DEWAAY III</h1>
+        <h1>Don DeWaay III</h1>
       </div>
       <div className="border-container">
         <div className="gradient">
